6cd9118ca67c4b168d587ce45609cb1f506409f5
Isaiah Wallace <unconfigured@null.spigotmc.org>
2018-09-25 21:35:46 -0700

    Remove lib files from repo

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // config params
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        LockBallToPaddle();
+        LaunchOnMouseClick();
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
